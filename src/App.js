b6bd7163ce65353c61ab8fb57d698f6e27312253
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-08-12 00:23:43 -0700

    import firebase in App.js, create a constructor in App.js.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { database } from './firebase';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      data: null
+    };
+  }
+
   render() {
     return (
       <div className="App">
