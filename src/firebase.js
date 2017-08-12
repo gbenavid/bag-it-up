b6b38e121fedf87529a7367df453416b00dec1a4
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-08-12 00:05:14 -0700

    import the SDK itself, add web setup from firebase with API key

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'; // this it the SDK itself
+
+const config = {
+    apiKey: "AIzaSyAJDuGvzzynalAwH23Lxh8F9tYBZnFjXu0",
+    authDomain: "bag-it-up-49697.firebaseapp.com",
+    databaseURL: "https://bag-it-up-49697.firebaseio.com",
+    projectId: "bag-it-up-49697",
+    storageBucket: "",
+    messagingSenderId: "484536466087"
+};
+firebase.initializeApp(config);
+
