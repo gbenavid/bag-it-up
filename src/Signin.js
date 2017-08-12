193720b5535ab4d671ccd569b673423c27ef43c1
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-08-12 02:47:41 -0700

    create button for signins

--- src/Signin.js
+++ src/Signin.js
@@ -1,2 +1,16 @@
 import React, { Component } from 'react';
-import { auth, googleAuthProvider } from './firebase';
+import { auth, googleAuthProvider, githubAuthProvider} from './firebase';
+
+class SignIn extends Component {
+    render() {
+        return (
+            <div className="SignIn">
+                <button onClick={()=> auth.signInWithRedirect(githubAuthProvider)}>
+                    Sign In
+                </button>
+            </div>
+        )
+    }
+}
+
+export default SignIn;
