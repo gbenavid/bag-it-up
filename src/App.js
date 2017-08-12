716613253a33a50a6c47f6336354f4717d52240a
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-08-12 04:16:51 -0700

    clean up code

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
 
   componentDidMount() {
     auth.onAuthStateChanged((currentUser) => { 
+      console.log("STATE CHANGED, ", currentUser)
       this.setState({ currentUser });
     })
   }
