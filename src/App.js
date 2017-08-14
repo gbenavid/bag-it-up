c1a6a4d7a159447d74c6c20fdc9e49bd7039170d
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-08-14 16:45:02 -0700

    clean up code and reorganize components

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ class App extends Component {
             ? <div>
                 <NewShoppingList
                   shoppingListsRef={this.shoppingListsRef} user={user}
+                  placeholderAttribute={"List name"}
                 />
                 {
                   shoppingLists &&
