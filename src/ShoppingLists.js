9d9a6bf6f8d22abc4d73eb02a751e0f927fdf1da
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-08-18 13:29:10 -0700

    create state to continiously check for new items in list

--- src/ShoppingLists.js
+++ src/ShoppingLists.js
@@ -1,26 +1,62 @@
 import React, { Component, PropTypes } from 'react';
+import { database } from './firebase';
 import ShoppingList from './ShoppingList';
 import ShowItems from './ShowItems';
+import LoopThroughItemsOnList from './LoopThroughItemsOnList';
 import map from 'lodash/map';
 
 class ShoppingLists extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      listedItems: []
+    }
     this.handleDelete = this.handleDelete.bind(this);
+    this.itemsListedRef = database.ref('/shopping_list').child(this.props.user.uid);//.child(this.props.placeToAppendTo);
   }
- 
+
   handleDelete(key) {
     const { shoppingListsRef } = this.props;
     shoppingListsRef.child(key).remove();
   }
+  componentWillMount(){
+
+  }
+  
+  toggleItems(key, marketClicked){
+    const properties = [];
+    this.itemsListedRef.on("value", function(snapshot) {
+      var marketObj = snapshot.val();
+      var keysInMarketObj = Object.keys(marketObj);
+      for(var i in keysInMarketObj){
+        for(var j in marketObj[keysInMarketObj[i]]){
+          if (marketObj[keysInMarketObj[i]].name ===  marketClicked ){
+            properties.push(marketObj[keysInMarketObj[i]][j].itemName);
+            // this.setState({listedItems: properties} );
+          }
+        }
+      }
+    }, function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    });
+    this.setState({listedItems: properties} );
+  }
 
   render () {
-    const { shoppingLists, user } = this.props;
+    const { shoppingLists, user, shoppingListsRef, properties } = this.props;
     return (
       <section>
-          { map(shoppingLists, (shoppingList, key) => (
-            <ShoppingList key={key} appendTo={key} user={user} {...shoppingList} handleDelete={ () => this.handleDelete(key) } />
-          )) }
+        {map(shoppingLists, (shoppingList, key) => (
+          
+          <ShoppingList key={key} 
+                        appendTo={key} 
+                        user={user}
+                        content={this.state.listedItems}
+                        toggleItems={ () => this.toggleItems(key,shoppingList.name)}
+                        handleDelete={ () => this.handleDelete(key)}
+                        {...shoppingList}
+          />
+        ))}
       </section>
     );
   }
@@ -29,7 +65,8 @@ class ShoppingLists extends Component {
 ShoppingLists.propTypes = {
   user: PropTypes.object,
   shoppingListsRef: PropTypes.object.isRequired,
-  shoppingLists: PropTypes.object
+  shoppingLists: PropTypes.object,
+  itemsListedRef:PropTypes.object
 };
 
 export default ShoppingLists;
