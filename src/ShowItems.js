dfe6da42126f555188e0c428cc1aa4195c823b69
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-08-18 13:30:06 -0700

    tidy code and pass content as props

--- src/ShowItems.js
+++ src/ShowItems.js
@@ -1,13 +1,17 @@
 import React, { Component, PropTypes } from 'react';
 import { database } from './firebase';
 import NewListItem from './NewListItem'
+import LoopThroughItemsOnList from './LoopThroughItemsOnList';
 import map from 'lodash/map';
 
 class ShowItems extends Component {
   constructor(props){
-    super(props);
+    super(props); 
+    this.itemsRef = database.ref('/shopping_list')
+                            .child(this.props.user.uid)
+                            .child(this.props.appendTo);
   }
-  this.itemsRef = database.ref('');
+
   render () {
     return (
       <div>
@@ -15,8 +19,10 @@ class ShowItems extends Component {
           user={this.props.user}
           appendTo={this.props.appendTo}
         />
-      
-        {/* map through all of the children in append to with the value of itemName*/}
+        <LoopThroughItemsOnList user={this.props.user} 
+                                itemsRef={this.itemsRef}
+                                content={this.props.content}
+        />
       </div>
     );
   }
