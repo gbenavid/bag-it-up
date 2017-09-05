9fe4212ca0abdf157a4ec64e589ed9eb7f9fc814
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-09-05 03:01:52 -0700

    attempt to set state in display()

--- src/ShoppingList.js
+++ src/ShoppingList.js
@@ -6,23 +6,41 @@ class ShoppingList extends Component {
   constructor(props){
     super(props);
     this.state = {
-      childVisible: false
+      childVisible: false,
+      newlyAppendedListItems: []
     }
   }
 /* Add conditional for listed items that also belong to the correct store to prevent unwanted items appearing under the wrong merchant */
-  display(storeItems){
-    database.ref('/shopping_list').child(this.props.user.uid).child(this.props.appendTo).on("value", function(snapshot){
-      console.log(snapshot.val());
+  display(storeItemsArray, ref){
+    var itemsToBeAdded = [];
+    ref.on("value", function(snapshot){
+      for(var i=0; i<snapshot.length-1;i+=1){
+        var childData = snapshot[i].val().itemName;
+        console.log(snapshot);
+        itemsToBeAdded.push(childData);
+      }
+      // console.log(itemsToBeAdded);
+        
+        
+        // if(itemsToBeAdded.includes(undefined)){
+        //   itemsToBeAdded = itemsToBeAdded.splice(0, itemsToBeAdded.length-1);        
+        // }
+        // this.state.  = itemsToBeAdded;
+
+      // this.setState({newlyAppendedListItems: itemsToBeAdded})
     })
-    // listen for changes in state
-      // set the state.
-    // return an accurate updated array based on changes in state
+      
+    // console.log(this.state);
+    // in your render statement you will pass your listedItems array into your ShowItems component
   }
 
   clicked(key, marketName){
     this.setState({childVisible: !this.state.childVisible});
-    this.props.toggleItems(key, marketName); // shows items belonging to this market
-    this.display(this.props.content);
+    this.props.toggleItems(key, marketName); // sets state of this.props.content
+    var listRef = database.ref('/shopping_list').child(this.props.user.uid).child(key); // which list/ market
+    
+    this.display(this.props.content, listRef);
+    // set state here instead of inside display();
   }
 
   render () {
